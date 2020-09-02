454f8d8dad7ba3a0e6ccc150d1bb6bf08020eb13
Glitch (lyrical-bristle-clementine) <none>
2020-09-02 14:00:14 +0000

    🚡🌠 Checkpoint ./js/bundle.js:13763590/48

--- js/bundle.js
+++ js/bundle.js
@@ -4330,7 +4330,7 @@
             id: "4",
             type: "4TDM",
             code: "glitch-worldwide-4",
-            at: p.glitch('closedregionffal'),
+            at: p.glitch('graceful-heady-chocolate'),
             prefer: !0,
            // featured: 1, // For Featured Status.
                       },
