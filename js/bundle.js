ff1c0c1804d9a970b502583dc704e2f3ab5956cb
Glitch (lyrical-bristle-clementine) <none>
2020-09-02 12:32:42 +0000

    🏬🌸 Checkpoint ./js/bundle.js:13763590/547

--- js/bundle.js
+++ js/bundle.js
@@ -3971,7 +3971,7 @@
           var c = [
             [
               "Tip: You can view and edit your keybinds in the options menu.",
-              "Tip: You can play on mobile by just going to arras.io on your phone!"
+              "Tip: You can play on mobile by just going to ka2-arras.glitch.me on your phone!"
             ],
             [
               "Tip: You can have the shield and health bar be separated by going to the options menu.",
@@ -3981,7 +3981,7 @@
               "Tip: You can create your own theme with the custom theme makerin the link on the options menu."
             ],
             [
-              "Teaming in FFA or FFA Maze is frowned upon, but when taken to the extremes, you can be punished.",
+              "Police Boosters can get you if you abuse the game!",
               "Witch hunting is when you continuously target someone and follow them. This is frowned upon, but when taken to the extremes, you can be punished.",
               "Multiboxing is when you use a script to control multiple tanks at the same time. This is considered CHEATING and will result in a ban."
             ]
